a38f0bf08d926be9549df2b0a476787d9e4cfe92
Ben Costerton <bencosterton@gmail.com>
2023-11-30 20:37:41 +0000

    Update ENF.py

--- ENF.py
+++ ENF.py
@@ -1,31 +1,32 @@
 import librosa
 import numpy as np
-import soundfile as sf
+import csv
 
-def extract_50Hz_signal(audio_file_path, output_file_path):
+def sample_and_save_50Hz_data(audio_file_path, output_csv_file_path):
+    # load the file and work out what it is (sample rate and length etc.)
     y, sr = librosa.load(audio_file_path, sr=None)
+    samples_per_second = int(sr / 50) 
+    num_seconds = int(len(y) / sr)
+    sampled_data = np.zeros(num_seconds)
 
-    fifty_hz_signal = y
+    # Calclate the 50Hz variation for each second in the audio file starting at 0
+    for i in range(num_seconds):
+        start_sample = i * samples_per_second
+        end_sample = (i + 1) * samples_per_second
+        sampled_data[i] = np.mean(y[start_sample:end_sample])
 
-    sf.write(output_file_path, fifty_hz_signal, sr)
+    time_values = np.arange(0, num_seconds)
+    absolute_values = np.full_like(sampled_data, 50.0) + sampled_data
+    formatted_absolute_values = np.round(absolute_values, 3) # format the fruency values to 3 decimal placs
+    data_to_save = np.column_stack((time_values, formatted_absolute_values))
 
-def sample_and_save_50Hz_data(audio_file_path, output_file_path):
-    y, sr = librosa.load(audio_file_path, sr=None)
+    # Put all the collected data in a csv file
+    with open(output_csv_file_path, 'w', newline='') as csvfile:
+        csvwriter = csv.writer(csvfile)
+        csvwriter.writerow(['t', 'f'])
+        csvwriter.writerows(data_to_save)
 
-    samples_per_second = int(sr / 50)
-    sampled_data = y[::samples_per_second]
+audio_file_path = 'C:\\Users\\benco\\Documents\\pyScripts\\ENFAnalaysis\\input_file.wav'
+sampled_50Hz_data_csv_file_path = 'C:\\Users\\benco\\Documents\\pyScripts\\ENFAnalaysis\\output_file.csv'
 
-    sampled_data = np.asarray(sampled_data, dtype=np.float32)
-
-    sampled_data_absolute = np.full_like(sampled_data, 50.0) + sampled_data
-
-    np.savetxt(output_file_path, sampled_data_absolute, fmt='%.3f')
-
-audio_file_path = 'C:\\Users\\benco\\Documents\\pyScripts\\ENFAnalaysis\\231010_1037.wav'
-extracted_50Hz_file_path = 'C:\\Users\\benco\\Documents\\pyScripts\\ENFAnalaysis\\extracted_50Hz_signal.wav'
-sampled_50Hz_data_file_path = 'C:\\Users\\benco\\Documents\\pyScripts\\ENFAnalaysis\\sampled_50Hz_data.txt'
-
-extract_50Hz_signal(audio_file_path, extracted_50Hz_file_path)
-
-sample_and_save_50Hz_data(extracted_50Hz_file_path, sampled_50Hz_data_file_path)
-
+sample_and_save_50Hz_data(audio_file_path, sampled_50Hz_data_csv_file_path)
