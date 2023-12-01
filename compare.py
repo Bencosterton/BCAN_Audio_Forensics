da0bb252ecb7b69b23e5cf61e0c5c2e49b845fe1
Ben Costerton <bencosterton@gmail.com>
2023-12-01 20:04:54 +0000

    Update compare.py

--- compare.py
+++ compare.py
@@ -1,10 +1,13 @@
 import pandas as pd
+import numpy as np
 
-def find_sequential_matching_dates(new_data_file, known_database_file, tolerance=0.003):
-    sampled_data = pd.read_csv(new_data_file, header=None, names=['ENF'], dtype={'ENF': float})
+def find_sequential_matching_dates(new_data_file, known_database_file, correlation_threshold=0.1, tolerance=0.003):
+    sampled_data = pd.read_csv(new_data_file, header=0, dtype={'f': float, 'd': float})
+    sampled_data['d'] = sampled_data['d'].replace(np.nan, 0.0)
     
     try:
-        known_database = pd.read_csv(known_database_file, skiprows=1, names=['Timestamp', 'ENF'], dtype={'ENF': float})
+        known_database = pd.read_csv(known_database_file, dtype={'f': float, 'd': float})
+        known_database['d'] = known_database['d'].replace(np.nan, 0.0)
     except ValueError:
         print("Error: Non-numeric values found in 'ENF' column of the known database.")
         return []
@@ -15,23 +18,23 @@ def find_sequential_matching_dates(new_data_file, known_database_file, tolerance
         current_match = [] 
 
         for j in range(len(sampled_data)):
-            new_data_value = sampled_data.loc[j, 'ENF']
-            known_data_value = known_database.loc[i + j, 'ENF']
+            new_data_value = sampled_data.loc[j, 'd']
+            known_data_value = known_database.loc[i + j, 'd']
 
             if abs(new_data_value - known_data_value) <= tolerance:
-                current_match.append(known_database.loc[i + j, 'Timestamp'])
+                current_match.append(known_database.loc[i + j, 'dtm'])
             else:
                 current_match = []  
                 break 
 
-        if len(current_match) == len(sampled_data):
+        if len(current_match) / len(sampled_data) >= correlation_threshold:
             matching_dates.extend(current_match)
             break 
 
     return matching_dates
 
-new_data_file = 'C:\\Users\\benco\\Documents\\pyScripts\\ENFAnalaysis\\_sampled_50Hz_data.txt'
-known_database_file = 'C:\\Users\\benco\\Documents\\pyScripts\\ENFAnalaysis\\knowndata.csv'
+new_data_file = 'C:\\Users\\benco\\Documents\\pyScripts\\ENFAnalaysis\\sample_database.csv'
+known_database_file = 'C:\\Users\\benco\\Documents\\pyScripts\\ENFAnalaysis\\known_database.csv'
 matching_dates = find_sequential_matching_dates(new_data_file, known_database_file)
 
 if matching_dates:
